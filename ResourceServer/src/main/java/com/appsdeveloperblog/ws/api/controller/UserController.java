42f5d9591011ba4e92e060e3dad814760ec25100
shrestha <shrestha@localhost>
2023-02-12 16:10:14 -0700

    1: Initial Commit

---
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.appsdeveloperblog.ws.api.controller;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.ws.api.response.UserRest;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("/status/check")
+	public String status() {
+		return "Working";
+
+	}
+	
+	//@Secured("ROLE_developer")
+	@PreAuthorize("hasAuthority('ROLE_monkey') or #id == #jwt.subject")
+	@DeleteMapping(path="/{id}")
+	public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return "Deleted user with id" + id + " and JWT subject" + jwt.getSubject();
+	}
+	
+	@PostAuthorize("returnObject.userId == #jwt.subject")
+	@GetMapping(path="/{id}")
+	public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return new UserRest("Nijjwal","Shrestha","47090bff-a983-46eb-bc80-d7d94623e944x");
+	}
+
+}
