90a4d039084abc8cbbb464e78a33b5f9a2bb067d
shrestha <shrestha@nijjwals-mbp>
2023-02-19 20:00:24 -0700

    10: Social login code

---
+++ SocialLoginWebClient/src/main/java/com/nijjwal/socialmvc/controller/IndexPageController.java
@@ -0,0 +1,15 @@
+package com.nijjwal.socialmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexPageController {
+
+	@GetMapping("/")
+	public String displayIndexPage(Model model) {
+		return "index";
+	}
+
+}
---
+++ SocialLoginWebClient/src/test/java/com/nijjwal/socialmvc/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nijjwal.socialmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
